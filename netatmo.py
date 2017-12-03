503540de992b5548382d787c854751a5a9d30cec
Bernardo M. Costa <bernardo.m.costa@tecnico.ulisboa.pt>
2017-12-03 22:59:10 +0000

    Signin method if logic split into functions

--- netatmo.py
+++ netatmo.py
@@ -11,6 +11,36 @@ CLIENT_SECRET = None
 
 app = Flask(__name__)
 
+def user_accepted_app():
+    code = r.args.get('code')
+    payload = {'grant_type': 'authorization_code',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'code': code,
+        'redirect_uri': 'http://localhost:5000/signin'}
+    try:
+        response = requests.post("https://api.netatmo.com/oauth2/token", data=payload)
+        response.raise_for_status()
+        access_token=response.json()["access_token"]
+        refresh_token=response.json()["refresh_token"]
+        scope=response.json()["scope"]
+        return "<p>Your access_token is:" + access_token + "</p>"
+
+    except requests.exceptions.HTTPError as error:
+        print(error.response.status_code, error.response.text)
+
+def ask_user_permission():
+    payload = {'client_id': CLIENT_ID,
+        'redirect_uri': "http://localhost:5000/signin",
+        'scope': 'read_station',
+        'state': uuid.uuid4().hex}
+    try:
+        response = requests.post("https://api.netatmo.com/oauth2/authorize", params=payload)
+        response.raise_for_status()
+        return redirect(response.url, code=302)
+    except requests.exceptions.HTTPError as error:
+        print(error.response.status_code, error.response.text)
+
 @app.route('/')
 def sign():
     return "<form action='/signin' method='get'><button type='submit'>Sign in</button></form>"
@@ -20,38 +50,13 @@ def sign():
 def signin():
     # Test if "code" is provided in get parameters (that would mean that user has already accepted the app and has been redirected here)
     if r.args.get('code'):
-        code = r.args.get('code')
-        payload = {'grant_type': 'authorization_code',
-           'client_id': CLIENT_ID,
-           'client_secret': CLIENT_SECRET,
-           'code': code,
-           'redirect_uri': 'http://localhost:5000/signin'}
-        try:
-            response = requests.post("https://api.netatmo.com/oauth2/token", data=payload)
-            response.raise_for_status()
-            access_token=response.json()["access_token"]
-            refresh_token=response.json()["refresh_token"]
-            scope=response.json()["scope"]
-            return "<p>Your access_token is:" + access_token + "</p>"
-
-        except requests.exceptions.HTTPError as error:
-            print(error.response.status_code, error.response.text)
+        return user_accepted_app()
     # Test if "error" is provided in get parameters (that would mean that the user has refused the app)
     elif r.args.get('error') == 'access_denied':
         return "The user refused to give access to his Netatmo data"
     # If "error" and "code" are not provided in get parameters: the user should be prompted to authorize your app
     else:
-
-        payload = {'client_id': CLIENT_ID,
-                'redirect_uri': "http://localhost:5000/signin",
-                'scope': 'read_station',
-                'state': uuid.uuid4().hex}
-        try:
-            response = requests.post("https://api.netatmo.com/oauth2/authorize", params=payload)
-            response.raise_for_status()
-            return redirect(response.url, code=302)
-        except requests.exceptions.HTTPError as error:
-            print(error.response.status_code, error.response.text)
+        return ask_user_permission()
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
