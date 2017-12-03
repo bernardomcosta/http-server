cb72fd2190904de3f20cd1ab0b784304e76c1f8a
Bernardo M. Costa <bernardo.m.costa@tecnico.ulisboa.pt>
2017-12-03 18:17:44 +0000

    Added check for import protection. Updated code to follow Python guidelines.

--- websocket.py
+++ websocket.py
@@ -11,20 +11,20 @@ class WebSocketHandler(http.server.BaseHTTPRequestHandler):
     
     # Generates a WebSocketAccept header value
     # Returns: A base64 encoded WebSocketAccept value, based on the given key
-    def _generateWebSocketAccept(self, key):
+    def _generate_websocket_accept(self, key):
         hash = hashlib.sha1((key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").encode('utf-8'))
         return base64.b64encode(hash.digest())
 
     # Generates headers for WebSocket handshake response
     # Arguments: Dictonary with the request headers
     # Response: Headers for the response of the WebSocket handshake
-    def _generateHeaders(self, requestHeaders):
+    def _generate_headers(self, requestHeaders):
         if self.WEBSOCKET_KEY not in requestHeaders:
             raise
 
         headers = {"Upgrade" : "websocket",
                     "Connection" : "Upgrade",
-                    "Sec-WebSocket-Accept" : self._generateWebSocketAccept(requestHeaders[self.WEBSOCKET_KEY])}
+                    "Sec-WebSocket-Accept" : self._generate_websocket_accept(requestHeaders[self.WEBSOCKET_KEY])}
 
         return headers
 
@@ -33,7 +33,7 @@ class WebSocketHandler(http.server.BaseHTTPRequestHandler):
 
         if self.path == "/ws":
             try:
-                headers = self._generateHeaders(self.headers)
+                headers = self._generate_headers(self.headers)
             except Exception as ex:
                 self.send_response(400)
                 self.end_headers()
@@ -47,7 +47,6 @@ class WebSocketHandler(http.server.BaseHTTPRequestHandler):
                 message = json.dumps({"status" : "success"})
 
                 self.wfile.write(bytearray(message, encoding="utf-8")) 
-       
         else:
             message_parts = [
                     "CLIENT VALUES:",
@@ -72,15 +71,14 @@ class WebSocketHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
 
             self.wfile.write(bytearray(message, encoding="utf-8"))
-        
-
         return
 
 # Main program
-PORT = 8000
+if __name__ == "__main__":
+    PORT = 8000
 
-Handler = WebSocketHandler
+    Handler = WebSocketHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
